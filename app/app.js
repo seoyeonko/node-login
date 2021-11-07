b1af8697627cb6034e2555031601ee8cf194998a
seoyeonko <seoyeon99o@naver.com>
2021-11-07 22:34:18 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // 모듈
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 // 라우팅
@@ -10,9 +11,11 @@ const home = require('./src/routes/home');
 // 앱 세팅
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-
 // use: 미들웨어 등록 메서드
 app.use(express.static(`${__dirname}/src/public`)); // __dirname: app.js 위치 반환
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // url을 통해 전달되는 데이터에 한글, 공백 등의 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+
 app.use('/', home);
 
 module.exports = app;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,43 @@
 'use strict';
 
-const home = (req, res) => {
-  res.render('home/index');
+const output = {
+  home: (req, res) => {
+    res.render('home/index');
+  },
+  login: (req, res) => {
+    res.render('home/login');
+  },
 };
 
-const login = (req, res) => {
-  res.render('home/login');
+// 인증을 위한 데이터
+const users = {
+  id: ['apple', 'orange', 'banana'],
+  psword: ['1234', '1234', '123456'],
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    console.log(id, psword);
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: '로그인에 실패하셨습니다.',
+    });
+  },
 };
 
 module.exports = {
-  home,
-  login,
+  output,
+  process,
 };
