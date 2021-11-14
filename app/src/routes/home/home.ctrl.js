eb63dec511a7797af5652aa40990750abbf5292b
seoyeonko <seoyeon99o@naver.com>
2021-11-14 23:45:18 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const UserStorage = require('../../models/UserStorage');
+const User = require('../../models/User');
 
 const output = {
   home: (req, res) => {
@@ -13,22 +13,9 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id,
-      psword = req.body.psword;
-
-    const users = UserStorage.getUsers('id', 'psword'); // UserStoarage 클래스의 id, psword 필드에 접근
-
-    const response = {}; // Object
-    if (users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
-      if (users.psword[idx] === psword) {
-        response.success = true; // { success: true }
-        return res.json(response);
-      }
-    }
-
-    response.success = false; // { success: false }
-    response.msg = '로그인에 실패하셨습니다.'; // { success: false, msg : '실패' }
+    const user = new User(req.body);
+    const response = user.login();
+    console.log(response);
     return res.json(response);
   },
 };
